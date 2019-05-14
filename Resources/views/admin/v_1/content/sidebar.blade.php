a9f15e3940ba502809fa3b5c73cc64a5327ad8cb
unknown <maldinuribrahim@gmail.com>
2019-05-15 02:14:57 +0700

    Fix Icon Sidebar

--- Resources/views/admin/v_1/content/sidebar.blade.php
+++ Resources/views/admin/v_1/content/sidebar.blade.php
@@ -1,6 +1,6 @@
 <li class="m-menu__item  {{url()->full() == action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\ModuleController@index') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"}>
     <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\ModuleController@index')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-cogwheel"></i>
+        <i class="m-menu__link-icon flaticon-squares-4"></i>
         <span class="m-menu__link-title"> 
             <span class="m-menu__link-wrap"> 
                 <span class="m-menu__link-text">
@@ -12,8 +12,7 @@
 </li>
 <li class="m-menu__item  {{url()->full() == action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\SettingController@create') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"}>
     <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\SettingController@create')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-squares-4
-"></i>
+        <i class="m-menu__link-icon flaticon-cogwheel"></i>
         <span class="m-menu__link-title"> 
             <span class="m-menu__link-wrap"> 
                 <span class="m-menu__link-text">
