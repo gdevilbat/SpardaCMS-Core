6207a9cc5d0871a0e7f7200f438c83aba4e54004
unknown <maldinuribrahim@gmail.com>
2019-05-24 17:31:44 +0700

    Fix Bug Menu Sidebar

--- Resources/views/admin/v_1/template.blade.php
+++ Resources/views/admin/v_1/template.blade.php
@@ -168,7 +168,9 @@
           <!-- BEGIN: Aside Menu -->
           <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
             <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-              {!!$menu!!}
+              @section('sidebar')
+                {!!$menu!!}
+              @show
             </ul>
            </div>
 
