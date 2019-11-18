3e2093060f14273b859aa63dabc55ff3f312fc38
unknown <maldinuribrahim@gmail.com>
2019-11-18 13:01:20 +0700

    Change Redirect URL After Login

--- Http/Middleware/RedirectIfAuthenticated.php
+++ Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/control/home');
+            return redirect('/control/dashboard');
         }
 
         return $next($request);
