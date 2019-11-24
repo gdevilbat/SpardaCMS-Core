06d467a4dac8b59ecf61f010c274e488eb3cdcf4
unknown <maldinuribrahim@gmail.com>
2019-11-25 00:53:38 +0700

    Fix Duplicate Rule

--- Http/Handler/UsersACLRepository.php
+++ Http/Handler/UsersACLRepository.php
@@ -16,26 +16,6 @@ class UsersACLRepository implements ACLRepository
         return \Auth::id();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function getRules(): array
-    {
-        if (\Auth::id() == 1) {
-            return [
-                ['disk' => env('FILESYSTEM_DRIVER'), 'path' => '*', 'access' => 2],
-            ];
-        }
-        
-        return [
-            ['disk' => env('FILESYSTEM_DRIVER'), 'path' => '/', 'access' => 1],                                  // main folder - read
-            ['disk' => env('FILESYSTEM_DRIVER'), 'path' => 'users', 'access' => 1],                              // only read
-            ['disk' => env('FILESYSTEM_DRIVER'), 'path' => 'users/'. \Auth::id(), 'access' => 1],        // only read
-            ['disk' => env('FILESYSTEM_DRIVER'), 'path' => 'users/'. \Auth::id() .'/*', 'access' => 2],  // read and write
-        ];
-    }
-    
     /*
      * Get ACL rules list for user
      *
