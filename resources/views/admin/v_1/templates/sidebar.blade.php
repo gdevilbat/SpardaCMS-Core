c41db7f29989356b169d4fedc2d9bd267f4cfddd
unknown <maldinuribrahim@gmail.com>
2019-11-26 18:44:14 +0700

    Menu Authorization

--- resources/views/admin/v_1/templates/sidebar.blade.php
+++ resources/views/admin/v_1/templates/sidebar.blade.php
@@ -1,36 +1,38 @@
-<li class="m-menu__item  {{Route::current()->getName() == 'filemanager' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
-    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\FileManagerController@index')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-symbol"></i>
-        <span class="m-menu__link-title"> 
-            <span class="m-menu__link-wrap"> 
-                <span class="m-menu__link-text">
-                    File Manager
-                </span>
-             </span>
-         </span>
-     </a>
-</li>
-<li class="m-menu__item  {{Route::current()->getName() == 'module' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
-    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\ModuleController@index')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-squares-4"></i>
-        <span class="m-menu__link-title"> 
-            <span class="m-menu__link-wrap"> 
-                <span class="m-menu__link-text">
-                    Module
-                </span>
-             </span>
-         </span>
-     </a>
-</li>
-<li class="m-menu__item  {{Route::current()->getName() == 'setting' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
-    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\SettingController@create')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-cogwheel"></i>
-        <span class="m-menu__link-title"> 
-            <span class="m-menu__link-wrap"> 
-                <span class="m-menu__link-text">
-                    Setting
-                </span>
-             </span>
-         </span>
-     </a>
-</li>
+@can('super-access')
+	<li class="m-menu__item  {{Route::current()->getName() == 'filemanager' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
+	    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\FileManagerController@index')}}" class="m-menu__link ">
+	        <i class="m-menu__link-icon flaticon-symbol"></i>
+	        <span class="m-menu__link-title"> 
+	            <span class="m-menu__link-wrap"> 
+	                <span class="m-menu__link-text">
+	                    File Manager
+	                </span>
+	             </span>
+	         </span>
+	     </a>
+	</li>
+	<li class="m-menu__item  {{Route::current()->getName() == 'module' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
+	    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\ModuleController@index')}}" class="m-menu__link ">
+	        <i class="m-menu__link-icon flaticon-squares-4"></i>
+	        <span class="m-menu__link-title"> 
+	            <span class="m-menu__link-wrap"> 
+	                <span class="m-menu__link-text">
+	                    Module
+	                </span>
+	             </span>
+	         </span>
+	     </a>
+	</li>
+	<li class="m-menu__item  {{Route::current()->getName() == 'setting' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
+	    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Core\Http\Controllers\SettingController@create')}}" class="m-menu__link ">
+	        <i class="m-menu__link-icon flaticon-cogwheel"></i>
+	        <span class="m-menu__link-title"> 
+	            <span class="m-menu__link-wrap"> 
+	                <span class="m-menu__link-text">
+	                    Setting
+	                </span>
+	             </span>
+	         </span>
+	     </a>
+	</li>
+@endcan
