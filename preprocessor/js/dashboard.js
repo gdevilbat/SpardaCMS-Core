32f946ec7e1fcc05129c0c328b89124a1a4704ca
maldinuribrahim <maldinuribrahim@gmail.com>
2021-08-01 20:05:19 +0700

    add Popupwindowfunction

--- preprocessor/js/dashboard.js
+++ preprocessor/js/dashboard.js
@@ -727,6 +727,12 @@ function showCountTextLength(textBox, e) {
     return true; 
 }
 
+window.popupWindow = function(url, windowName, win, w, h) {
+    const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
+    const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
+    return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+}
+
 window. debounce = function(func, wait, immediate) {
     var timeout;
     return function() {
