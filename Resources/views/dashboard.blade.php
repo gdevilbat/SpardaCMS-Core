40d7cecc64395e72b7691d7f8938e076b9273775
maldinuribrahim <maldinuribrahim@gmail.com>
2022-11-22 14:32:57 +0700

    Script move javascript

--- Resources/views/dashboard.blade.php
+++ Resources/views/dashboard.blade.php
@@ -22,8 +22,8 @@
     <div id="main">
         <router-view></router-view>
     </div>
+    <script src="{{ mix('/js/manifest.js') }}"></script>
+    <script src="{{ mix('/js/vendor.js') }}"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
-<script src="{{ mix('/js/manifest.js') }}"></script>
-<script src="{{ mix('/js/vendor.js') }}"></script>
-<script src="{{ mix('/js/app.js') }}"></script>
 </html>
